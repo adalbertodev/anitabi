ccf71b6ea6e5f1ea31e86ced7ece2958037f84ca
Adalberto Perdomo Abreu <adalberto.devs@gmail.com>
2026-07-08 12:50:39 +0100

    chore: actualización de dependencias: coreKtx, junitVersion, espressoCore, lifecycleRuntimeKtx, activityCompose y composeBom. Subida compileSdk a 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,7 @@ plugins {
 
 android {
     namespace = "dev.adalbertodev.anitabi"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "ev.adalbertodev.anitabi"
