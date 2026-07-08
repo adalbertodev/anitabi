99b9a23b84d8f532c6797f7569339d62443db4f8
Adalberto Perdomo Abreu <adalberto.devs@gmail.com>
2026-07-08 11:16:05 +0100

    chore: init

---
+++ README.md
@@ -0,0 +1,5 @@
+# AniTabi
+
+Cliente Android minimalista de AniList. En desarrollo.
+
+Kotlin · Jetpack Compose · Apollo GraphQL · OAuth2 Implicit Grant
---
+++ app/src/main/java/ev/adalbertodev/anitabi/MainActivity.kt
@@ -0,0 +1,47 @@
+package ev.adalbertodev.anitabi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ev.adalbertodev.anitabi.ui.theme.AniTabiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AniTabiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AniTabiTheme {
+        Greeting("Android")
+    }
+}
